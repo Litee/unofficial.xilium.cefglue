2d7161e80fd37b2dd97ee3f0f2f0573afc105cd9
Litee <lipatkin@gmail.com>
2017-12-17 00:21:04 +0300

    Suppressed console question in build-and-package-all.cmd

--- build-and-package-all.cmd
+++ build-and-package-all.cmd
@@ -1,10 +1,10 @@
 mkdir target\net40
-del CefGlue\bin\Release\*.*
+del /Q CefGlue\bin\Release\*.*
 call build-net40.cmd
 copy CefGlue\bin\Release\*.* target\net40
 
 mkdir target\net45
-del CefGlue\bin\Release\*.*
+del /Q CefGlue\bin\Release\*.*
 call build-net45.cmd
 copy CefGlue\bin\Release\*.* target\net45
 
